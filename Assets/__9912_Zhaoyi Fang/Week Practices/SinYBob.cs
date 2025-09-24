2fb30d8a310b125f119a278280e3788840820348
Avalokitesvara9 <405482285@qq.com>
2025-09-24 14:26:36 +1000

    practice on week 2

---
+++ Assets/__9912_Zhaoyi Fang/Week Practices/SinYBob.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SinYBob : MonoBehaviour
+{
+    public Vector3 Startposition;
+    public Vector3 sinOffset;
+    public float alpha;
+    public float sinValue;
+    public float rangeFactor;
+    public float bobSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Startposition = transform.localPosition;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sinValue = Mathf.Sin(alpha * Mathf.Deg2Rad);
+        sinOffset.y = sinValue * rangeFactor;
+        transform.localPosition = Startposition + sinOffset;
+        alpha += bobSpeed * Time.deltaTime;
+    }
+}
